75e283878688fe4d42fe7d90370be86437755fbe
surbhigupta1012 <gupta.surb@husky.neu.edu>
2015-10-26 16:36:56 -0400

    :privacyPolicy

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+
+app.get("/privacy", function (req, res) {
+    res.sendfile('privacyPolicy.html');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
@@ -9,5 +14,6 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get("/", function (req, res) {
     res.sendfile(__dirname + '/public/Hello.html');
 });
+
 
 app.listen(port, ipaddress);
