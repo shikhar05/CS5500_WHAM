eac50b3f2e680bc7dcdecf856df46aaba35e511a
surbhigupta1012 <gupta.surb@husky.neu.edu>
2015-11-05 16:41:31 -0500

    Eventful search Service

--- public/js/Service.js
+++ public/js/Service.js
@@ -1,8 +1,6 @@
-﻿
-app.factory("MyService", function ($http) {
-
+﻿app.factory("MyService", function ($http) {
+    //Google Maps
     var position ={lat:null,lon:null}
-
     var initUserPosition = function (callback) {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (pos) {
@@ -10,19 +8,33 @@ app.factory("MyService", function ($http) {
                 position.lon = pos.coords.longitude;
                 callback('ok');
             });
-
         } else {
             callback('Geolocation is not supported by this browser.');
         }
     };
-
     var getUserPosition = function () {
         return position;
     };
 
+    //Event search
+    var searchEvent = function (title, callback) {
+        $http.jsonp("http://api.eventful.com/rest/events/search?...&keywords=books&location=San+Diego&date=Future")
+        .success(callback);
+    }
+    var events = [];
+    var addToList = function (event) {
+        events.push(event);
+    }
+    var getEvents = function () {
+        return events;
+    }
+
+    //Return block
     return {
+        search: searchEvent,
+        addToList: addToList,
+        getEvents: getEvents,
         initUserPosition: initUserPosition,
         getUserPosition: getUserPosition
     }
-
 });
