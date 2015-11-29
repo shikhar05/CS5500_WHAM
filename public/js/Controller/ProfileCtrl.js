1ff0a5c1cf0ff68745adaa707ccb939f920d8f87
Monisha Singh <msingh28@ccs.neu.edu>
2015-11-28 21:15:12 -0500

    change password

--- public/js/Controller/ProfileCtrl.js
+++ public/js/Controller/ProfileCtrl.js
@@ -3,21 +3,28 @@ app.controller("ProfileCtrl", function ($scope, LoginService, $location) {
 
     $scope.activeTabIndex = 0;
 
+    $scope.userProfile = null;
+
+    $scope.editPassword = {
+        oldPassword: null,
+        newPassword: null,
+        confirmPassword: null,
+        errors: {}
+    };
+
     $scope.init = function () {
         $scope.profileOptionsToggle = false;
     }
 
+    $scope.$watch(function () {
+        return LoginService.getCurrentUSerProfile();
+    }, function (response) {
+        $scope.userProfile = response;
+    }, true);
+
     $scope.goToHome = function () {
         $location.url('/home');
     };
-
-    $scope.userProfile = null;
-
-    $scope.$watch(
-        function () { return LoginService.getCurrentUSerProfile() },
-        function (resp) {
-            $scope.userProfile = resp;
-        }, true);
 
     $scope.setProfileOptionsToggle = function () {
         $scope.profileOptionsToggle = !$scope.profileOptionsToggle;
@@ -34,4 +41,48 @@ app.controller("ProfileCtrl", function ($scope, LoginService, $location) {
     $scope.openTab = function (index) {
         $scope.activeTabIndex = index;
     }
+
+   
+
+    //*************************************changePassword*************************************
+
+    $scope.changePassword = function () {
+      //  $scope.validateOldPassword();
+        $scope.validateNewPassword();
+        $scope.validateNewConfirmPassword();
+
+        if (Object.keys($scope.editPassword.errors).length == 0) {
+
+           // var newUSer = $scope.register;
+           // LoginService.register(newUSer, function (msg) {
+              //  if (msg == 'ok') {
+                //    $location.url("/home");
+               // }
+          //  });
+        };
+    };
+   // $scope.validateOldPassword = function () {
+    //}
+    $scope.validateNewPassword = function () {
+        if ($scope.editPassword.newPassword == null || $scope.editPassword.newPassword == "") {
+            $scope.editPassword.errors.newPassword = "Please choose a Password.";
+        } else if ($scope.editPassword.newPassword.length < 8 || $scope.editPassword.newPassword.length > 15) {
+            $scope.editPassword.errors.newPassword = "Password must be a atlease 8 characters and atmost 15 characters";
+        } else {
+            if ($scope.editPassword.confirmPassword != null || $scope.editPassword.confirmPassword != "") {
+                $scope.validateNewConfirmPassword();
+            }
+            delete $scope.editPassword.errors.newPassword;
+        };
+    };
+
+    $scope.validateNewConfirmPassword = function () {
+        if ($scope.editPassword.confirmPassword == null || $scope.editPassword.confirmPassword == "") {
+            $scope.editPassword.errors.confirmPassword = "Please confirm Password.";
+        } else if ($scope.editPassword.confirmPassword != $scope.editPassword.newPassword) {
+            $scope.editPassword.errors.confirmPassword = "Passwords does not match.";
+        } else {
+            delete $scope.editPassword.errors.confirmPassword;
+        };
+    };
 });
